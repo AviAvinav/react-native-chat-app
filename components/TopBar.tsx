9a78fe4391f2306087a85b69e8be62da06c58c8f
Avi Avinav <aviavinav2004@gmail.com>
2022-10-25 22:56:57 +0530

    add sign out

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,14 +1,18 @@
-import { View, Text } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import Icon from './atoms/Icon';
+import { View, Text } from "react-native";
+import Icon from "./atoms/Icon";
+import { supabase } from "../lib/supabase";
 
 const TopBar = () => {
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
   return (
     <View className="flex flex-row justify-between items-center px-5 py-2">
       <Text className="text-2xl">ChitChat</Text>
       <View className="flex flex-row">
         <Icon name="search" />
         <Icon name="edit" />
+        <Icon onPress={() => signOut()} name="log-out" />
       </View>
     </View>
   );
--- components/atoms/Icon.tsx
+++ components/atoms/Icon.tsx
@@ -1,15 +1,19 @@
-import { View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 interface IconProps {
-  name: 'search' | 'edit';
+  name: "search" | "edit" | "log-out";
+  onPress?: () => void;
 }
 
-const Icon = ({ name }: IconProps) => {
+const Icon = ({ name, onPress }: IconProps) => {
   return (
-    <View className="bg-gray-300 p-2 ml-4 rounded-full flex items-center justify-center">
+    <TouchableOpacity
+      onPress={onPress}
+      className="bg-gray-300 p-2 ml-4 rounded-full flex items-center justify-center"
+    >
       <Feather name={name} size={22} color="black" />
-    </View>
+    </TouchableOpacity>
   );
 };
 
